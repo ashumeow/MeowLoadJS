ea3f25e5a156fc97a4098db21f3a67543e307cee
Aswini S <ashumeow@live.com>
2015-01-31 13:06:58 +0530

    Update MeowLoadJS.js

--- MeowLoadJS.js
+++ MeowLoadJS.js
@@ -1,5 +1,35 @@
 function MeowLoadJS() {
+	// Main file
 	'use strict';
+
+	// MeowJS CSS Loader
+	function meowLoaderCSS() {
+	var xx = window.document.createElement("link");
+	var ref = before || window.document.getElementsByTagName("script")[0];
+	var meowStyleSheets = window.document.styleSheets;
+	xx.rel = "stylesheet";
+	xx.href = href;
+	xx.media = "specific"; // fetches without blocking render
+	xx.onLoad = callback || function() {};
+	ref.parentNode.insertBefore(xx, ref);
+	function toggleMedia() {
+		var defined;
+		for(var m = 0; m < meowStyleSheets.length; m++) {
+			if(meowStyleSheets[m].href && meowStyleSheets[m].href.indexOf(href) > -1) {
+				defined = true;
+			}
+		}
+		if(defined) {
+			xx.media = media || "all";
+		} else {
+			setTimeout(toggleMedia);
+		}
+	}
+	toggleMedia();
+	return xx;
+	}
+
 	//
 	// Still more to code!
+	//
 }
